6515e1e8656b4414f660dc17a196b929cc96ddd6
Tom <tom.sandt@telekom.de>
2025-06-12 12:55:52 +0200

    added apiService.js

--- src/features/customer/customerService.js
+++ src/features/customer/customerService.js
@@ -1,6 +1,6 @@
 import {addData, deleteData, fetchData, updateData} from "@/services/apiService";
 
-export const fetchCustomers = () => fetchData("customers");
-export const updateCustomers = (id, data) => updateData("customers", id, data);
-export const deleteCustomers = (id) => deleteData("customers", id);
-export const addCustomer = (data) => addData("customers", data);
+export const fetchCustomers = () => fetchData("customer");
+export const updateCustomers = (id, data) => updateData("customer", id, data);
+export const deleteCustomers = (id) => deleteData("customer", id);
+export const addCustomer = (data) => addData("customer", data);
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,30 +1,32 @@
+const BASE_URL = 'http://localhost:8080/frontend/v1';
+
 export async function fetchData(endpoint) {
-    const response = await fetch("/customers.json"/*`/api/${endpoint}`*/)
+    const response = await fetch(`${BASE_URL}/${endpoint}`)
     if (!response.ok) throw new Error("Error while fetching data.");
     return await response.json();
 }
 
-export async function updateData(endpoint, id, updatedObject) {
-    const response = await fetch(`/api/${endpoint}/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify(updatedObject),
-    });
-    console.log(`PUT request to: /api/${endpoint}/${id}`, updatedObject);
-    return await response.json();
-}
-
-export async function deleteData(endpoint, id) {
-    await fetch(`/api/${endpoint}/${id}`, { method: "DELETE" });
-    console.log(`DELETE request to: /api/${endpoint}/${id}`);
-}
-
 export async function addData(endpoint, newObject) {
-    const response = await fetch(`/api/${endpoint}`, {
+    const response = await fetch(`${BASE_URL}/${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newObject),
     });
-    console.log(`POST request to: /api/${endpoint}`, newObject);
+    console.log(`POST request to: ${BASE_URL}/${endpoint}`, newObject);
     return await response.json();
+}
+
+export async function updateData(endpoint, id, updatedObject) {
+    const response = await fetch(`${BASE_URL}/${endpoint}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(updatedObject),
+    });
+    console.log(`PUT request to: ${BASE_URL}/${endpoint}`, updatedObject);
+    return await response.json();
+}
+
+export async function deleteData(endpoint, id) {
+    await fetch(`${BASE_URL}/${endpoint}/${id}`, { method: "DELETE" });
+    console.log(`DELETE request to: ${BASE_URL}/${endpoint}`);
 }
