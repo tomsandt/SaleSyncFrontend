d403f1ef6a1aa0b2c76607170718a4c19809fbc7
Tom <tom.sandt@telekom.de>
2025-06-12 14:50:26 +0200

    deployment test

--- src/features/customer/CustomerTable.jsx
+++ src/features/customer/CustomerTable.jsx
@@ -9,16 +9,16 @@ import {addCustomer, deleteCustomers, fetchCustomers, updateCustomers} from "@/f
 
 const columns = [
     { field: "id", headerName: "ID", width: 100 },
-    { field: "firstName", headerName: "First Name", width: 200, editable: true },
-    { field: "lastName", headerName: "Last Name", width: 250, editable: true },
+    { field: "firstName", headerName: "First name", width: 200, editable: true },
+    { field: "lastName", headerName: "Last name", width: 250, editable: true },
     { field: "street", headerName: "Street", width: 150, editable: true },
     { field: "zipCode", headerName: "ZIP Code", width: 150, editable: true },
     { field: "city", headerName: "City", width: 150, editable: true },
-    { field: "mail", headerName: "E-Mail", width: 150, editable: true },
+    { field: "mail", headerName: "Mail", width: 150, editable: true },
     { field: "phone", headerName: "Phone", width: 150, editable: true },
 ];
 
-const fields = ["firstName", "lastName", "street", "zipCode", "city", "mail", "phone"]
+const fields = ["First name", "Last name", "Street", "ZIP", "City", "Mail", "Phone"]
 
 export default function CustomerTable() {
     const [open, setOpen] = useState(false);
