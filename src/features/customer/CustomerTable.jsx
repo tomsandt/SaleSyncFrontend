41c7df92377c0a7f3899b1ac7ad8a2eb083c29e0
Tom <tom.sandt@telekom.de>
2025-06-12 14:26:59 +0200

    changed to camelCase

--- src/features/customer/CustomerTable.jsx
+++ src/features/customer/CustomerTable.jsx
@@ -9,16 +9,16 @@ import {addCustomer, deleteCustomers, fetchCustomers, updateCustomers} from "@/f
 
 const columns = [
     { field: "id", headerName: "ID", width: 100 },
-    { field: "first_name", headerName: "First Name", width: 200, editable: true },
-    { field: "last_name", headerName: "Last Name", width: 250, editable: true },
+    { field: "firstName", headerName: "First Name", width: 200, editable: true },
+    { field: "lastName", headerName: "Last Name", width: 250, editable: true },
     { field: "street", headerName: "Street", width: 150, editable: true },
-    { field: "zip_code", headerName: "ZIP Code", width: 150, editable: true },
+    { field: "zipCode", headerName: "ZIP Code", width: 150, editable: true },
     { field: "city", headerName: "City", width: 150, editable: true },
-    { field: "email", headerName: "Email", width: 150, editable: true },
+    { field: "mail", headerName: "E-Mail", width: 150, editable: true },
     { field: "phone", headerName: "Phone", width: 150, editable: true },
 ];
 
-const fields = ["first_name", "last_name", "street", "zip_code", "city", "email", "phone"]
+const fields = ["firstName", "lastName", "street", "zipCode", "city", "mail", "phone"]
 
 export default function CustomerTable() {
     const [open, setOpen] = useState(false);
